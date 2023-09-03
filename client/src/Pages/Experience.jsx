88062ba5202f92ef71a0b3f415f7a64201a2ca22
William Mori <wmori@ucsb.edu>
2023-09-03 00:42:34 -0700

    edit tablet experience section

--- client/src/Pages/Experience.jsx
+++ client/src/Pages/Experience.jsx
@@ -28,60 +28,24 @@ const Experience = () => {
 
     return (
       <React.Fragment>
-        {!isTabletScreen ?
-        <Grid style={{height: '100vh', paddingLeft: '200px', paddingTop: '200px'}} id="experience">
-          <Container ref={ref} style={{width: '700px', paddingLeft: '0px'}} className={`fade-in-up ${inView ? 'is-visible' : ''}`}>
+        
+        <Grid style={{height: '100vh', paddingLeft: isTabletScreen ? '0px' : '200px', paddingTop: '200px'}} id="experience">
+          <Container ref={ref} style={{maxWidth: '700px', paddingLeft: '0px'}} className={`fade-in-up ${inView ? 'is-visible' : ''}`}>
             <Container style={{display: 'flex', alignItems: 'center', marginBottom: '30px', paddingLeft: '0px'}}>
               <Typography variant="h5" style={{color: theme.palette.primary.textPrimary}}>02.&nbsp;&nbsp;</Typography>
               <Typography sx={{ fontSize: '30px', fontWeight: 'bold' }} style={{color: 'rgb(210, 215, 240)'}}>Roles I've Worked</Typography>
               <div style={{borderBottom: `1px solid rgb(140, 145, 175, 0.3)`, width: '260px', marginLeft: '20px'}}></div>
             </Container>
           </Container>
-          <Container ref={ref} style={{paddingLeft: '0px', width: '700px', display: 'flex'}} className={`fade-in-up ${inView ? 'is-visible' : ''}`}>
-            <RolesSelector vertical={true} role={role} changeRole={changeRole} />
+          <Container ref={ref} style={{paddingLeft: '0px', maxWidth: '700px', display: 'flex', flexDirection: isSmallTablet ? 'column' : ''}} className={`fade-in-up ${inView ? 'is-visible' : ''}`}>
+            <RolesSelector vertical={isSmallTablet ? false : true} role={role} changeRole={changeRole} />
             <div className={`fade-in-out visible`}>
               <RolesDetails role={role} />
             </div>
           </Container>
         </Grid>
-        :
-        !isSmallTablet ?
-        <Grid style={{height: '80vh', paddingLeft: '0px', paddingTop: '200px'}} id="experience">
-            <Container ref={ref} style={{maxWidth: '1000px', display: 'flex', flexDirection: 'column'}} className={`fade-in-up ${inView ? 'is-visible' : ''}`}>
-                    <Container style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-                        <Container style={{display: 'flex', alignItems: 'center', marginBottom: '30px', justifyContent: 'center', position: 'relative', right: '30px'}}>
-                            <Typography variant="h5" style={{color: theme.palette.primary.textPrimary}}>02.&nbsp;&nbsp;</Typography>
-                            <Typography sx={{ fontSize: '30px', fontWeight: 'bold' }} style={{color: 'rgb(210, 215, 240)'}}>Roles I've Worked</Typography>
-                            <div style={{borderBottom: `1px solid rgb(140, 145, 175, 0.3)`, width: '260px', marginLeft: '20px'}}></div>
-                        </Container>
-                        <RolesSelector vertical={false} role={role} changeRole={changeRole} />
-                        <div style={{maxWidth: '500px'}} className={`fade-in-out visible`}>
-                          <RolesDetails role={role} />
-                        </div>
-                    </Container>
-                    
-            </Container>
-          
-        </Grid> 
-        :
-        <Grid style={{height: '80vh', paddingLeft: '0px', paddingTop: '200px'}} id="experience">
-            <Container ref={ref} style={{maxWidth: '1000px', display: 'flex', flexDirection: 'column'}} className={`fade-in-up ${inView ? 'is-visible' : ''}`}>
-                    <Container style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-                        <Container style={{display: 'flex', alignItems: 'center', marginBottom: '30px', justifyContent: 'center', position: 'relative', right: '30px'}}>
-                            <Typography variant="h5" style={{color: theme.palette.primary.textPrimary}}>02.&nbsp;&nbsp;</Typography>
-                            <Typography sx={{ fontSize: '30px', fontWeight: 'bold' }} style={{color: 'rgb(210, 215, 240)'}}>Roles I've Worked</Typography>
-                            <div style={{borderBottom: `1px solid rgb(140, 145, 175, 0.3)`, width: '260px', marginLeft: '20px'}}></div>
-                        </Container>
-                        <RolesSelector vertical={false} role={role} changeRole={changeRole} />
-                        <div style={{maxWidth: '500px'}} className={`fade-in-out visible`}>
-                          <RolesDetails role={role} />
-                        </div>
-                    </Container>
-                    
-            </Container>
-          
-        </Grid> 
-        }
+       
+        
       </React.Fragment>
         
         
