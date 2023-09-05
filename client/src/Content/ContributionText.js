f44da4bd71885d814da3698d76302557274bbc10
William Mori <wmori@ucsb.edu>
2023-09-04 20:42:57 -0700

    more content changes

--- client/src/Content/ContributionText.js
+++ client/src/Content/ContributionText.js
@@ -5,7 +5,7 @@ export const ContributionText = {
     "Companies": {
         "header": "Metalinguist",
         "subHeader": "Companies Page",
-        "details": "Build companies feature / companies page within the Metalinguist portal with the ability to add new companies and provide card view of existing companies with their respective data. Implement techniques for load speed optimization and integrate with AWS S3 to display company logos.",
+        "details": "Build companies feature / companies page within the Metalinguist portal with the ability to add new companies and provide card view of existing companies with their respective data. Implement techniques for load speed optimization and integrate with AWS S3 to upload / download company logos.",
         "tech_stack": ["React.js", "MUI", "Express.js", "AWS S3", "Node.js"],
         "headerLink": "https://metalinguist.com"
     },
@@ -19,7 +19,7 @@ export const ContributionText = {
     "CookBook_Social": {
         "header": "CookBook Social",
         "subHeader": "Features",
-        "details": "Design and implementation of homepage recipe feed with various filtering options. Implement 'likes' functionality / popup modal on recipe posts and create Express.js REST API for managing recipe and user data flow throughout the app.",
+        "details": "Design and implementation of homepage recipe feed with various filtering options. Implement 'likes' functionality / popup modal on recipe posts and develop backend Express.js REST API using Firebase cloud functions to manage recipe and user data flow throughout the app.",
         "tech_stack": ["React.js", "Tailwind", "Node.js", "Express.js", "Firebase"],
         "headerLink": "https://cookbook-social.herokuapp.com/login"
     },
--- client/src/Content/RolesDetailsContent.js
+++ client/src/Content/RolesDetailsContent.js
@@ -14,7 +14,7 @@ export const rolesDetailsContent = {
     "UCSB": {
         "header": "Frontend Developer",
         "date": "Dec 2020 - Mar 2021",
-        "details": ["Create and design new page data tables and implement filtering options using React",
+        "details": ["Implement new page data tables with various filtering options using React",
                     "Redesign certain parts of the admin portal with the goal of improving user experience",
                     "Create mockup designs in Figma",
                     "Emphasized 90%+ code coverage in testing using Jest",
@@ -24,7 +24,7 @@ export const rolesDetailsContent = {
         "header": "Full-Stack Developer",
         "date": "Dec 2022 - Mar 2023",
         "details": ["Implement custom reusable React components utilizing ES6 JavaScript and Tailwind CSS",
-                    "Implement backend Express.js API services using Firebase to manage data flow throughout the app",
+                    "Develop backend Express.js API services using Firebase to manage data flow throughout the app",
                     "Lead daily scrum meetings to ensure steady progress was being made towards both our MVP and final release deadlines",
                     "Conduct code reviews and responsible for merging PR's"]
     }
