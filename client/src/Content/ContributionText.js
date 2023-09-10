eebab3d9b75511f6361b8200331ad4bbded26d3f
William Mori <wmori@ucsb.edu>
2023-09-09 17:06:30 -0700

    update contribution content text

--- client/src/Content/ContributionText.js
+++ client/src/Content/ContributionText.js
@@ -4,7 +4,7 @@
 export const ContributionText = {
     "Companies": {
         "header": "Metalinguist",
-        "subHeader": "Companies Page",
+        "subHeader": "Companies Feature",
         "details": "Build companies feature / companies page within the Metalinguist portal with the ability to add new companies and provide card view of existing companies with their respective data. Implement techniques for load speed optimization and integrate with AWS S3 / DynamoDB",
         "tech_stack": ["React.js", "MUI", "Express.js", "AWS S3", "DynamoDB"],
         "headerLink": "https://metalinguist.com"
@@ -18,7 +18,7 @@ export const ContributionText = {
     },
     "CookBook_Social": {
         "header": "CookBook Social",
-        "subHeader": "Features",
+        "subHeader": "Full-Stack Features",
         "details": "Design and implementation of homepage recipe feed with various filtering options. Implement 'likes' functionality / popup modal on recipe posts and develop backend Express.js REST API using Firebase cloud functions to manage recipe and user data flow throughout the app.",
         "tech_stack": ["React.js", "Tailwind", "Node.js", "Express.js", "Firebase"],
         "headerLink": "https://cookbook-social.herokuapp.com/login"
