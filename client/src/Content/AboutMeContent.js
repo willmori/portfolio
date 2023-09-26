a3ad1d8507235380e4218a0486d934366d73f78a
William Mori <wmori@ucsb.edu>
2023-09-25 21:18:56 -0700

    update aboutme to include languages

--- client/src/Content/AboutMeContent.js
+++ client/src/Content/AboutMeContent.js
@@ -4,5 +4,5 @@ export const aboutMeContent = {
     "paragraph1": 'Hello! My name is William and I am a recent graduate in Computer Science from the University of California, Santa Barbara. I specialize in frontend development coupled with knowledge and experience in backend development (APIs, databases, third party services such as AWS) which I believe is fundamental for any frontend developer to be successful.',
     "paragraph2": "My interest in software development started back in 2020 when I took my first course involving working on the UCSB admin portal alongside a team of students. It gave me a great introduction to the field, and I found enjoyment in bringing awesome ideas to life through web development.",
     "paragraph3": `Fast-forward to today, and I've had the priviledge to work on some industry level products. In 2022, I led a team of developers to build our very own CookBook Social media web app, and most recently in 2023 I have been contracted by a tech startup called Metalinguist to work on a new revolutionary translation management system.`,
-    "paragraph4": 'Throughout my undergrad in computer science, these are some of the notable courses I took:'
+    "paragraph4": 'Throughout my undergrad in computer science, these are some of the notable courses I took utilizing C++, Python, Java, SQL, and React:'
 }
