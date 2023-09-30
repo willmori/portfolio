05f027d5b31eb7ccdb6931012f28eacb824291d3
William Mori <wmori@ucsb.edu>
2023-09-29 20:22:23 -0700

    update contribution text

--- client/src/Content/ContributionText.js
+++ client/src/Content/ContributionText.js
@@ -5,28 +5,28 @@ export const ContributionText = {
     "Companies": {
         "header": "Metalinguist",
         "subHeader": "Companies Feature",
-        "details": "Build companies feature / companies page within the Metalinguist portal with the ability to add new companies and provide card view of existing companies with their respective data. Implement techniques for load speed optimization and integrate with AWS S3 / DynamoDB",
-        "tech_stack": ["React.js", "MUI", "Express.js", "AWS S3", "DynamoDB"],
+        "details": "Build companies feature / page within the Metalinguist portal, providing a card view of existing companies with their respective data and the ability to add new companies. Implement techniques for load speed optimization and integrate with AWS S3 / DynamoDB",
+        "tech_stack": ["React.js", "MUI", "Express.js", "S3", "DynamoDB"],
         "headerLink": "https://metalinguist.com"
     },
     "Playwright": {
         "header": "Metalinguist",
         "subHeader": "Testing Automation",
-        "details": "Setup and integration of Playwright framework for end-to-end testing. Implement test suite for the Metalinguist Onboarding flow and create cloud util functions for DynamoDB / AWS route53 subdomain cleanup.",
-        "tech_stack": ["Playwright", "AWS route53", "DynamoDB", "Node.js"],
+        "details": "Setup and integration of Playwright framework for end-to-end testing. Implement test suite for the Metalinguist Onboarding flow and create cloud util functions for AWS DynamoDB / Route53 subdomain cleanup.",
+        "tech_stack": ["Playwright", "Route53", "DynamoDB", "Node.js"],
         "headerLink": "https://metalinguist.com"
     },
     "Provisioning": {
         "header": "Metalinguist",
         "subHeader": "Domain Provisioning",
-        "details": "Implement full-stack feature to provide real-time monitoring of new domain provisioning. Custom animated React loading bar synchronized with route53 update status by utilizing the Node.js AWS SDK",
-        "tech_stack": ["React.js", "AWS route53", "Node.js"],
+        "details": "Implement full-stack feature to provide real-time monitoring of new domain provisioning. Custom animated React loading bar synchronized with Route53 update status by utilizing the Node.js AWS SDK",
+        "tech_stack": ["React.js", "Route53", "Node.js", "Express.js"],
         "headerLink": "https://metalinguist.com"
     },
     "CookBook_Social": {
         "header": "CookBook Social",
         "subHeader": "Full-Stack Features",
-        "details": "Design and implementation of homepage recipe feed with various filtering options. Implement 'likes' functionality / popup modal on recipe posts and develop backend Express.js REST API using Firebase cloud functions to manage recipe and user data flow throughout the app.",
+        "details": "Design and implementation of homepage recipe feed with various filtering options. Implement 'likes' functionality / popup modal on recipe posts and develop backend Express.js REST APIs using Firebase cloud functions to manage recipe and user data flow throughout the app.",
         "tech_stack": ["React.js", "Tailwind", "Node.js", "Express.js", "Firebase"],
         "headerLink": "https://cookbook-social.herokuapp.com/login"
     },
