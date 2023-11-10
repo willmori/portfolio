53c5e7cf0627ba1dcb8d381bedff9a67cf0f095f
William Mori <wmori@ucsb.edu>
2023-11-10 15:27:19 -0800

    copy skill and skill tiles

--- client/src/Components/SkillTile.jsx
+++ client/src/Components/SkillTile.jsx
@@ -41,3 +41,52 @@ function SkillTile({ logo, width, height, type }) {
 }
 
 export default SkillTile;
+
+
+
+/*
+
+import React from "react";
+import { Typography, useTheme } from '@mui/material';
+import { useInView } from 'react-intersection-observer';
+
+function SkillTile({ logo, width, height, type }) { 
+
+    const theme = useTheme();
+
+    const [ref, inView] = useInView({
+        triggerOnce: false, // Trigger only once
+        threshold: 0.2, // Percentage of component visibility to trigger
+    });
+
+    return (
+        <div>
+            <div ref={ref} className={`fade-in-up ${inView ? 'is-visible' : ''}`} style={{ width: '230px', height: '230px', display: 'flex', flexDirection: 'column', 
+                        justifyContent: 'center', alignItems: 'center', borderRadius: '15px', position: 'relative',}}>
+                <div style={{
+                        position: 'absolute',
+                        top: '50%', 
+                        left: '50%',
+                        transform: 'translate(-50%, -50%)',
+                        width: '110%',
+                        height: '110%',
+                        background: 'radial-gradient(circle, rgba(255,255,255,0.5) 10%, rgba(255,255,255,0) 65%)',
+                        zIndex: 5, // Place it behind the logo
+                    }}></div>
+                <div style={{position: 'relative', display: 'flex', justifyContent: 'center', alignItems: 'center', position: 'relative', right: type === 'mui' ? '5px' : ''}}>
+                    {type === 'express' ? <Typography style={{display: 'inline', fontSize: '35px', color: '#878787', position: 'relative', 
+                                                                top: '5px', right: '3px', fontWeight: 100, letterSpacing: '-2px'}}>Express</Typography> : ''}
+                    <img src={logo} style={{width: width, height: height, position: 'relative',
+                            zIndex: 6,}} alt="" />
+                    {type === 'mui' ? <Typography style={{display: 'inline', fontSize: '35px', color: 'rgb(80,175,250)'}}>MUI</Typography> : ''}
+                    {type === 'javascript' ? <Typography style={{display: 'inline', fontSize: '35px', color: 'rgb(215,185,75)', position: 'relative', right: '25px'}}>ES6+</Typography> : ''}
+                </div>
+                
+            </div>
+
+        </div>
+    )
+}
+
+export default SkillTile;
+*/
--- client/src/Pages/Skills.jsx
+++ client/src/Pages/Skills.jsx
@@ -52,3 +52,63 @@ const Skills = () => {
 }
 
 export default Skills;
+
+
+
+/*
+
+import React, {useState, useEffect} from 'react';
+import { Button, Grid, Container, Typography, List, ListItem, ListItemText, Paper } from '@mui/material';
+import { useTheme } from '@mui/material';
+import { useMediaQuery } from '@mui/material';
+import { useInView } from 'react-intersection-observer';
+import SkillTile from '../Components/SkillTile';
+import ReactLogo from '../assets/React.png';
+import MUILogo from '../assets/MUI.png';
+import NodeLogo from '../assets/Node.png';
+import ExpressLogo from '../assets/Express.webp';
+import Express from '../assets/Express.png';
+import JavaScriptLogo from '../assets/JavaScript.png';
+import PythonLogo from '../assets/Python.png';
+import CLogo from '../assets/C++.png';
+import SQLLogo from '../assets/SQL.png';
+
+const Skills = () => {
+
+    const theme = useTheme();
+    const isSmallTablet = useMediaQuery((theme) => theme.breakpoints.down('sm'));
+    const isTabletScreen = useMediaQuery((theme) => theme.breakpoints.down('md'));
+ 
+    const [ref, inView] = useInView({
+        triggerOnce: false, // Trigger only once
+        threshold: 0.3, // Percentage of component visibility to trigger
+    });
+
+    return (
+        <div id="skills" style={{marginTop: !isTabletScreen ? '280px' : '50px'}}>
+            <Container ref={ref} className={`fade-in-up ${inView ? 'is-visible' : ''}`} style={{display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '30px'}}>
+                <Typography variant="h5" style={{color: theme.palette.primary.textPrimary}}>02.&nbsp;&nbsp;</Typography>
+                {isSmallTablet ? <Typography sx={{ fontSize: '30px', fontWeight: 'bold' }} style={{color: 'rgb(210, 215, 240)'}}>Skills</Typography>
+                : <Typography sx={{ fontSize: '30px', fontWeight: 'bold' }} style={{color: 'rgb(210, 215, 240)'}}>Technical Skills</Typography>}
+                
+                <div style={{borderBottom: `1px solid rgb(140, 145, 175, 0.3)`, width: '260px', marginLeft: '20px'}}></div>
+            </Container>
+            <Container style={{maxWidth: '1100px'}}>
+                <Grid container align="center" style={{}}>
+                    <Grid style={{marginBottom: '40px'}} item xs={12} sm={6} md={3} lg={3} xl={3}>{<SkillTile logo={ReactLogo} width={'180px'} height={'auto'} />}</Grid>
+                    <Grid style={{marginBottom: '40px'}} item xs={12} sm={6} md={3} lg={3} xl={3}>{<SkillTile logo={MUILogo} width={'auto'} height={'70px'} type={'mui'} />}</Grid>
+                    <Grid style={{marginBottom: '40px'}} item xs={12} sm={6} md={3} lg={3} xl={3}>{<SkillTile logo={NodeLogo} width={'215px'} height={'auto'} />}</Grid>
+                    <Grid style={{marginBottom: '40px'}} item xs={12} sm={6} md={3} lg={3} xl={3}>{<SkillTile logo={Express} width={'auto'} height={'40px'} type={'express'} />}</Grid>
+                    <Grid style={{marginBottom: '40px'}} item xs={12} sm={6} md={3} lg={3} xl={3}>{<SkillTile logo={JavaScriptLogo} width={'auto'} height={'60px'} type={'javascript'} />}</Grid>
+                    <Grid style={{marginBottom: '40px'}} item xs={12} sm={6} md={3} lg={3} xl={3}>{<SkillTile logo={PythonLogo} width={'180px'} height={'auto'} />}</Grid>
+                    <Grid style={{marginBottom: '40px'}} item xs={12} sm={6} md={3} lg={3} xl={3}>{<SkillTile logo={CLogo} width={'120px'} height={'auto'} />}</Grid>
+                    <Grid style={{marginBottom: '40px'}} item xs={12} sm={6} md={3} lg={3} xl={3}>{<SkillTile logo={SQLLogo} width={'120px'} height={'auto'} />}</Grid>
+                </Grid>
+            </Container>
+        </div>
+        
+    );
+}
+
+export default Skills;
+*/
